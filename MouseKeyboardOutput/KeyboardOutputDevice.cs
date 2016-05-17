201298610d117bd81b5f4ac6d090810ce74dbaec
André Ferreira <andre@geckonm.com>
2016-05-18 00:26:48 +0100

    Fixing channels

--- MouseKeyboardOutput/KeyboardOutputDevice.cs
+++ MouseKeyboardOutput/KeyboardOutputDevice.cs
@@ -5,12 +5,12 @@ namespace MouseKeyboardOutput
 {
     internal class KeyboardOutputDevice
     {
-        public Button Ctrl { get; set; } = new Button("Ctrl", DataFlowDirection.Output);
-        public Button Shift { get; set; } = new Button("Shift", DataFlowDirection.Output);
-        public Button Enter { get; set; } = new Button("Enter", DataFlowDirection.Output);
-        public Button Backspace { get; set; } = new Button("Backspace", DataFlowDirection.Output);
-        public Button Esc { get; set; } = new Button("Esc", DataFlowDirection.Output);
-        public Button AltEnter { get; set; } = new Button("Alt & Enter", DataFlowDirection.Output);
-        public Button OnScreenKeyboard { get; set; } = new Button("Show/Hide On-Screen Keyboard", DataFlowDirection.Output);
+        public Button Ctrl { get; set; } = new Button("Ctrl", DataFlowDirection.Input);
+        public Button Shift { get; set; } = new Button("Shift", DataFlowDirection.Input);
+        public Button Enter { get; set; } = new Button("Enter", DataFlowDirection.Input);
+        public Button Backspace { get; set; } = new Button("Backspace", DataFlowDirection.Input);
+        public Button Esc { get; set; } = new Button("Esc", DataFlowDirection.Input);
+        public Button AltEnter { get; set; } = new Button("Alt & Enter", DataFlowDirection.Input);
+        public Button OnScreenKeyboard { get; set; } = new Button("Show/Hide On-Screen Keyboard", DataFlowDirection.Input);
     }
 }
--- MouseKeyboardOutput/MouseOutputDevice.cs
+++ MouseKeyboardOutput/MouseOutputDevice.cs
@@ -5,23 +5,23 @@ namespace MouseKeyboardOutput
 {
     internal class MouseOutputDevice
     {
-        public JoyAxis CursorX { get; set; } = new JoyAxis("CursorX", DataFlowDirection.Output);
+        public JoyAxis CursorX { get; set; } = new JoyAxis("CursorX", DataFlowDirection.Input);
 
-        public JoyAxis CursorY { get; set; } = new JoyAxis("CursorY", DataFlowDirection.Output);
+        public JoyAxis CursorY { get; set; } = new JoyAxis("CursorY", DataFlowDirection.Input);
 
-        public JoyAxis DeltaX { get; set; } = new JoyAxis("DeltaX", DataFlowDirection.Output);
+        public JoyAxis DeltaX { get; set; } = new JoyAxis("DeltaX", DataFlowDirection.Input);
 
-        public JoyAxis DeltaY { get; set; } = new JoyAxis("DeltaY", DataFlowDirection.Output);
+        public JoyAxis DeltaY { get; set; } = new JoyAxis("DeltaY", DataFlowDirection.Input);
 
-        public Button LeftButton { get; set; } = new Button("Left button", DataFlowDirection.Output);
+        public Button LeftButton { get; set; } = new Button("Left button", DataFlowDirection.Input);
 
-        public Button RightButton { get; set; } = new Button("Right button", DataFlowDirection.Output);
-        public Button MiddleButton { get; set; } = new Button("Middle button", DataFlowDirection.Output);
-        public Button FourthButton { get; set; } = new Button("Fourth button", DataFlowDirection.Output);
-        public Button FifthButton { get; set; } = new Button("Fifth button", DataFlowDirection.Output);
-        public Button ScrollUp { get; set; } = new Button("Scroll wheel up", DataFlowDirection.Output);
-        public Button ScrollDown { get; set; } = new Button("Scroll wheel down", DataFlowDirection.Output);
-        public Button ScrollRight { get; set; } = new Button("Scroll wheel right", DataFlowDirection.Output);
-        public Button ScrollLeft { get; set; } = new Button("Scroll wheel left", DataFlowDirection.Output);
+        public Button RightButton { get; set; } = new Button("Right button", DataFlowDirection.Input);
+        public Button MiddleButton { get; set; } = new Button("Middle button", DataFlowDirection.Input);
+        public Button FourthButton { get; set; } = new Button("Fourth button", DataFlowDirection.Input);
+        public Button FifthButton { get; set; } = new Button("Fifth button", DataFlowDirection.Input);
+        public Button ScrollUp { get; set; } = new Button("Scroll wheel up", DataFlowDirection.Input);
+        public Button ScrollDown { get; set; } = new Button("Scroll wheel down", DataFlowDirection.Input);
+        public Button ScrollRight { get; set; } = new Button("Scroll wheel right", DataFlowDirection.Input);
+        public Button ScrollLeft { get; set; } = new Button("Scroll wheel left", DataFlowDirection.Input);
     }
 }
